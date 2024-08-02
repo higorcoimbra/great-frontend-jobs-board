eb2d049ef8cbfebd581f5c950605da500d591d65
Higor Coimbra <higor1308@gmail.com>
2024-08-02 18:53:45 -0300

    feat: get job ids from hacker news api

--- jobs-board/src/app/app.component.ts
+++ jobs-board/src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { JobService } from '../services/job.service';
+import { Observable } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AsyncPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'jobs-board';
+  jobIds$: Observable<string[]>;
+
+  constructor(jobService: JobService) {
+    this.jobIds$ = jobService.getJobIds();
+  }
 }
